d06dfda1661646733ac44ad18a3bb9147370debd
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-05-24 03:17:11 +0000

    添加查询所有建筑物
    git-svn-id: svn://202.107.200.162:8002/repo/mcs@5827 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/web/demo/org/jeecgframework/web/demo/service/impl/test/JeecgMinidaoServiceImpl.java
+++ src/web/demo/org/jeecgframework/web/demo/service/impl/test/JeecgMinidaoServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.lierda.web.entity.ZBuildingEntity;
+
 /**
  * Minidao例子
  * @author fancq
@@ -85,9 +87,9 @@ public class JeecgMinidaoServiceImpl implements JeecgMinidaoServiceI {
 	}
 
 	@Override
-	public List<String> getAllBuildingId() {
+	public List<ZBuildingEntity> getAllBuildingId(String flag) {
 		// TODO Auto-generated method stub
-		return jeecgMinidaoDao.getAllBuildingId();
+		return jeecgMinidaoDao.getAllBuildingId(flag);
 	}
 
 }
