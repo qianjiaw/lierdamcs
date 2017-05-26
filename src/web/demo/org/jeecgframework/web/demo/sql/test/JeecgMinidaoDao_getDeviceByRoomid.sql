defd94f30a628b2cedf1891be42f36fd399ab411
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-05-26 09:30:32 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6015 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/web/demo/org/jeecgframework/web/demo/sql/test/JeecgMinidaoDao_getDeviceByRoomid.sql
+++ src/web/demo/org/jeecgframework/web/demo/sql/test/JeecgMinidaoDao_getDeviceByRoomid.sql
@@ -2,6 +2,8 @@ select d.id,d.name
 from z_room r
 join z_ddc_rfbp p
 on p.roomid=r.id
+join z_ddc ddc
+on ddc.ddcmac=p.ddcmac
 join z_device d
-on d.ddcId=p.ddcid
+on d.ddcId=ddc.ddcid
 where r.id=:roomid
