16a7bf66d1acad6d5fd34fb1b2aa1f96952ca363
xuxudong <xuxudong@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-05-26 07:56:19 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6008 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/service/impl/ZDdcRfbpServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lierda.web.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lierda.web.service.ZDdcRfbpServiceI;
+import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
+
+@Service("zDdcRfbpService")
+@Transactional
+public class ZDdcRfbpServiceImpl extends CommonServiceImpl implements ZDdcRfbpServiceI {
+	
+}
