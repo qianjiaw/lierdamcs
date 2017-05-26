25340f55f13dcdd88dd16ab761dd468529acd72a
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-05-26 02:32:43 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@5966 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/com/lierda/web/service/impl/DdcServiceImpl.java
+++ src/com/lierda/web/service/impl/DdcServiceImpl.java
@@ -1,28 +1,13 @@
 package com.lierda.web.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.lierda.web.service.DdcServiceI;
-
 import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
-import org.jeecgframework.web.demo.dao.test.JeecgMinidaoDao;
 
 @Service("ddcService")
 @Transactional
 public class DdcServiceImpl extends CommonServiceImpl implements DdcServiceI {
-
-//	@Autowired
-//	private DdcDao ddcDao;
-//
-//	@Override
-//	public Integer getCount() {
-//		// TODO Auto-generated method stub
-//		return ddcDao.getCount();
-//	}
 	
-	
-
-
 }
