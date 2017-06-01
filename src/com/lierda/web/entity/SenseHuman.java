e439e32fafbbd3e4ffb80284bba9bbe4165f4e7d
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:43:01 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6135 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/SenseHuman.java
@@ -0,0 +1,30 @@
+package com.lierda.web.entity;
+/**
+ * 人感
+ * @author qianjiawei
+ *
+ */
+public class SenseHuman {
+	/**人感状态*/
+	private String status;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public SenseHuman(String status) {
+		super();
+		this.status = status;
+	}
+
+	public SenseHuman() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
