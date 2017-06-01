1e497ad1a727f6f5f82d9a7f24ddb88ff7f1a219
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:55:18 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6142 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/FloorHeating.java
@@ -0,0 +1,35 @@
+package com.lierda.web.entity;
+/**
+ * 地暖
+ * @author qianjiawei
+ *
+ */
+public class FloorHeating {
+	/**地暖状态*/
+	private String status;
+	/**开着的地暖数量*/
+	private int count;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public FloorHeating(String status, int count) {
+		super();
+		this.status = status;
+		this.count = count;
+	}
+	public FloorHeating() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
