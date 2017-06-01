e626e11e6fe24edb29fb6e98af8d829c793d3a1c
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:55:10 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6140 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/Bgm.java
@@ -0,0 +1,36 @@
+package com.lierda.web.entity;
+/**
+ * 音乐
+ * @author qianjiawei
+ *
+ */
+public class Bgm {
+	/**音乐状态*/
+	private String status;
+	/**音乐开启数量*/
+	private int count;
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public Bgm(String status, int count) {
+		super();
+		this.status = status;
+		this.count = count;
+	}
+	public Bgm() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
