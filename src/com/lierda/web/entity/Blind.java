eab6436b931d6ca3500a84546f52cdda69ca10e4
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:55:14 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6141 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/Blind.java
@@ -0,0 +1,30 @@
+package com.lierda.web.entity;
+/**
+ * 窗帘
+ * @author  qianjiawei
+ *
+ */
+public class Blind {
+	/**窗帘状态*/
+	private String status;
+
+	public Blind(String status) {
+		super();
+		this.status = status;
+	}
+
+	public Blind() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
