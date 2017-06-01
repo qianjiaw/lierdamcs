65c25a3f51a703bb53b5ce2acff6dabcae7e3039
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:43:17 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6137 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/Lock.java
@@ -0,0 +1,37 @@
+package com.lierda.web.entity;
+/**
+ * 门锁
+ * @author  qianjiawei
+ *
+ */
+public class Lock {
+	/**门锁状态*/
+	private String status;
+	/**开启门锁数量*/
+	private int count;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public Lock(String status, int count) {
+		super();
+		this.status = status;
+		this.count = count;
+	}
+	
+	public Lock() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
