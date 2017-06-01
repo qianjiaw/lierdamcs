d80769d6e6a86139ac459b02f7ae78fabedcd625
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:55:05 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6139 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/AirConditioner.java
@@ -0,0 +1,53 @@
+package com.lierda.web.entity;
+/**
+ * 空调
+ * @author qianjiawei
+ */
+public class AirConditioner {
+	/**空调状态*/
+	private String status;
+	/**空调模式*/
+	private String mode;
+	/**空调温度*/
+	private String temperature;
+	/**空调风速*/
+	private String windSpeed;
+	public AirConditioner(String status, String mode, String temperature,
+			String windSpeed) {
+		super();
+		this.status = status;
+		this.mode = mode;
+		this.temperature = temperature;
+		this.windSpeed = windSpeed;
+	}
+	public AirConditioner() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getMode() {
+		return mode;
+	}
+	public void setMode(String mode) {
+		this.mode = mode;
+	}
+	public String getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+	public String getWindSpeed() {
+		return windSpeed;
+	}
+	public void setWindSpeed(String windSpeed) {
+		this.windSpeed = windSpeed;
+	}
+	
+	
+}
