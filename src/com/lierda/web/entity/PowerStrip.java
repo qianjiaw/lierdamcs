f0de815498d1d44cb1beb350eda7da56f10995db
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:55:22 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6143 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/PowerStrip.java
@@ -0,0 +1,35 @@
+package com.lierda.web.entity;
+/**
+ * 插座
+ * @author  qianjiawei
+ *
+ */
+public class PowerStrip {
+	/**插座状态*/
+	private String status;
+	/**打开的插座数量*/
+	private int count;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public PowerStrip(String status, int count) {
+		super();
+		this.status = status;
+		this.count = count;
+	}
+	public PowerStrip() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
