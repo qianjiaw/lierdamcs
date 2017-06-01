d0fe61d8713d8e8e55c37285ad65c1e90465d1f0
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-01 02:43:09 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6136 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/Light.java
@@ -0,0 +1,35 @@
+package com.lierda.web.entity;
+/**
+ * 照明
+ * @author  qianjiawei
+ *
+ */
+public class Light {
+	/**照明状态*/
+	private String status;
+	/**开着的灯数量*/
+	private int count;
+	public Light(String status, int count) {
+		super();
+		this.status = status;
+		this.count = count;
+	}
+	public Light() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
