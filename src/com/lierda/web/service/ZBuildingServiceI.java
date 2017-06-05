6bb38fbb14fc2cd9465edd7d6036ea24dd9be356
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-05 06:17:53 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6221 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/com/lierda/web/service/ZBuildingServiceI.java
+++ src/com/lierda/web/service/ZBuildingServiceI.java
@@ -1,7 +1,14 @@
 package com.lierda.web.service;
 
+import java.util.List;
+
 import org.jeecgframework.core.common.service.CommonService;
+
+import com.lierda.web.entity.PowerRecordingEntity;
 
 public interface ZBuildingServiceI extends CommonService{
 
+	public Double AvgPower(List<Double> powerArray);
+	
+	public Double[] getPower(List<PowerRecordingEntity> entities,long timeStart);
 }
