70478ad42a26711f9f0ddfe0901c8ab550aee63c
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-07 01:27:09 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6293 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/com/lierda/web/service/ZFloorServiceI.java
+++ src/com/lierda/web/service/ZFloorServiceI.java
@@ -9,7 +9,7 @@ import com.lierda.web.resultEntity.JsonResult;
 
 public interface ZFloorServiceI extends CommonService{
 	
-	public Map<String, Object> getDeviceStatus(String floorid);
+	public Map<String, Object> getDeviceStatus(String floorid,String flag);
 	
 	public String getSql(List<JsonResult> list,String sql);
 	
