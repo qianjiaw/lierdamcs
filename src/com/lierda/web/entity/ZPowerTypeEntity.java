9675ab9fff4e56b329e9929e7f14fc4d6b9f07f4
xuxudong <xuxudong@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-14 06:29:17 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6399 e790cb50-a368-4814-8953-9b4d82950c0f

---
+++ src/com/lierda/web/entity/ZPowerTypeEntity.java
@@ -0,0 +1,91 @@
+package com.lierda.web.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.SequenceGenerator;
+
+/**   
+ * @Title: Entity
+ * @Description: 计量类型
+ * @author zhangdaihao
+ * @date 2017-06-07 14:39:28
+ * @version V1.0   
+ *
+ */
+@Entity
+@Table(name = "z_power_type", schema = "")
+@DynamicUpdate(true)
+@DynamicInsert(true)
+@SuppressWarnings("serial")
+public class ZPowerTypeEntity implements java.io.Serializable {
+	/**id*/
+	private java.lang.Integer id;
+	/**计量统计类型*/
+	private java.lang.Integer type;
+	/**计量设备id*/
+	private java.lang.String devicemac;
+	
+	/**
+	 *方法: 取得java.lang.Integer
+	 *@return: java.lang.Integer  id
+	 */
+	
+	@Id
+	@GeneratedValue(generator = "paymentableGenerator")
+	@GenericGenerator(name = "paymentableGenerator", strategy = "uuid")
+	@Column(name ="ID",nullable=false,precision=10,scale=0)
+	public java.lang.Integer getId(){
+		return this.id;
+	}
+
+	/**
+	 *方法: 设置java.lang.Integer
+	 *@param: java.lang.Integer  id
+	 */
+	public void setId(java.lang.Integer id){
+		this.id = id;
+	}
+	/**
+	 *方法: 取得java.lang.Integer
+	 *@return: java.lang.Integer  计量统计类型
+	 */
+	@Column(name ="TYPE",nullable=false,precision=10,scale=0)
+	public java.lang.Integer getType(){
+		return this.type;
+	}
+
+	/**
+	 *方法: 设置java.lang.Integer
+	 *@param: java.lang.Integer  计量统计类型
+	 */
+	public void setType(java.lang.Integer type){
+		this.type = type;
+	}
+	/**
+	 *方法: 取得java.lang.String
+	 *@return: java.lang.String  计量设备id
+	 */
+	@Column(name ="DEVICEMAC",nullable=false,length=20)
+	public java.lang.String getDevicemac(){
+		return this.devicemac;
+	}
+
+	/**
+	 *方法: 设置java.lang.String
+	 *@param: java.lang.String  计量设备id
+	 */
+	public void setDevicemac(java.lang.String devicemac){
+		this.devicemac = devicemac;
+	}
+}
