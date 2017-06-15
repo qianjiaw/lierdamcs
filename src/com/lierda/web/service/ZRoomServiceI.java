7aaf091d7379902e66fa62a2310fe77d2c411287
qianjw <qianjw@e790cb50-a368-4814-8953-9b4d82950c0f>
2017-06-15 05:04:11 +0000

    git-svn-id: svn://202.107.200.162:8002/repo/mcs@6424 e790cb50-a368-4814-8953-9b4d82950c0f

--- src/com/lierda/web/service/ZRoomServiceI.java
+++ src/com/lierda/web/service/ZRoomServiceI.java
@@ -1,7 +1,13 @@
 package com.lierda.web.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.jeecgframework.core.common.service.CommonService;
+
+import com.lierda.web.resultEntity.RoomDeviceSta;
 
 public interface ZRoomServiceI extends CommonService{
 
+	public List<RoomDeviceSta> getDeviceDetail(String roomid,String type);
 }
